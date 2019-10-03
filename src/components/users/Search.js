43ef8cb5b913d5150b496b5c793bb3acdf903acf
ShinHyungJune <ssa4141@naver.com>
2019-10-03 15:59:36 +0900

    n

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,34 +1,28 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
+const Search = ({searchUsers, clearUsers}) => {
+	let [text, setText] = useState('');
 
-class Search extends Component {
-	state = {
-		text: ""
-	};
-
-	onChange = (e) => {
+	let onChange = (e) => {
 		this.setState({text: e.target.value});
 	};
 
-	onSubmit = (e) => {
+	let onSubmit = (e) => {
 		e.preventDefault();
 
 		this.props.searchUsers(this.state.text);
 
 		this.setState({text: ""});
 	};
-	render() {
-		const {searchUsers, clearUsers} = this.props;
 
-		return (
-			<div>
-				<form onSubmit={this.onSubmit} className="form">
-					<input type="text" name="text" placeholder="type word" value={this.state.text} onChange={this.onChange}/>
-					<button>Search</button>
-					<button type="button" onClick={clearUsers}>Clear</button>
-				</form>
-			</div>
-		);
-	}
-}
+	return (
+		<div>
+			<form onSubmit={this.onSubmit} className="form">
+				<input type="text" name="text" placeholder="type word" value={this.state.text} onChange={this.onChange}/>
+				<button>Search</button>
+				<button type="button" onClick={clearUsers}>Clear</button>
+			</form>
+		</div>
+	);
+};
 
 export default Search;
